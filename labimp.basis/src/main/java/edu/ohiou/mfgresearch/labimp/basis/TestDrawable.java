9eaaf7a2148438d0e3f6c9101db94add843b50a5
anibalcc97 <ac985015@ohio.edu>
2022-01-14 13:26:31 -0500

    for desktop

--- labimp.basis/src/main/java/edu/ohiou/mfgresearch/labimp/basis/TestDrawable.java
+++ labimp.basis/src/main/java/edu/ohiou/mfgresearch/labimp/basis/TestDrawable.java
@@ -11,103 +11,86 @@ import javax.swing.JPanel;
 
 public class TestDrawable implements Drawable2D {
 
-	@Override
 	public void settCanvas(Draw2DPanel canvas) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public JPanel geettCanvas() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void paintComponent(Graphics g) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void paintComponent(Graphics2D g) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public GraphicsConfiguration geetGraphicsConfig() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void makeDrawSets() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public LinkedList<Shape> geetDrawList() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public LinkedList<Shape> geetFillList() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public LinkedList<DrawString> geetStringList() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void generateImageList() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void addButtonOptions(int options) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void removeButtonOptions(int options) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void setNeedUpdate(boolean needUpdate) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public Collection giveSelectables() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Point2D geettPosition() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void settPosition(Point2D point) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public String toToolTipString() {
 		// TODO Auto-generated method stub
 		return null;
