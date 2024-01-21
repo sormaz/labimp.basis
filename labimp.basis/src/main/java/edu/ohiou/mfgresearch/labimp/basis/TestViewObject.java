b4ab723f7e8bc78a9b2597fa1da33062db420fca
sormaz <sormaz@gmail.com>
2024-01-21 16:22:25 -0500

    show how simple is to extend ViewObject

---
+++ labimp.basis/src/main/java/edu/ohiou/mfgresearch/labimp/basis/TestViewObject.java
@@ -0,0 +1,11 @@
+package edu.ohiou.mfgresearch.labimp.basis;
+
+public class TestViewObject extends ViewObject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestViewObject tvo = new TestViewObject();
+		tvo.display("showing TestViewObject");
+	}
+
+}
